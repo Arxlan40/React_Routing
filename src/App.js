3cdafe40c115a4d44c0d029bdd091cf56c8d87f9
ARSALAN UMAR <64530489+Arxlan40@users.noreply.github.com>
2021-04-21 22:54:39 +0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Blog from "./containers/Blog/Blog";
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  render() {
+  render () {
     return (
+      // <BrowserRouter basename="/my-app">
       <BrowserRouter>
-        {" "}
         <div className="App">
           <Blog />
         </div>
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,37 +1,55 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
-import Posts from "../Blog/Posts/Posts";
-import "./Blog.css";
-import { Route, Link } from "react-router-dom";
-import NewPost from "../Blog/NewPost/NewPost";
+import './Blog.css';
+import Posts from './Posts/Posts';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 class Blog extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-                <Link
-                  to={{
-                    pathname: "/New-Post",
-                    hash: "#search",
-                    search: "?some-true",
-                  }}
-                >
-                  New Post
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Route path="/" exact render={() => <Posts />} />
-        <Route path="/New-Post" render={() => <NewPost />} />{" "}
-      </div>
-    );
-  }
+    state = {
+        auth: true
+    }
+
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Blog;
